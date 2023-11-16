df6877dfdf50eb49566958fc8dbd24a009c195aa
Jeff-WuYo <62562200+Jeff-WuYo@users.noreply.github.com>
2023-11-16 22:12:12 +0800

    add locale and ntp config

--- rough_draft/post-install.sh
+++ rough_draft/post-install.sh
@@ -1,15 +1,16 @@
-#!/bin/zsh
+#!/bin/sh
 ln -sf /usr/share/zoneinfo/Asia/Taipei /etc/localtime
-# edit /locale.gen
-# locale-gen
+# locale configuration
+sed -i.bkp 's/#en_US.UTF-8/en_US.UTF-8/; s/#zh_TW.UTF-8/zh_TW.UTF-8/' /etc/locale.gen && locale-gen
 touch /etc/hostname && echo "archmobo" > /etc/hostname
-touch /etc/doas.conf && echo "permit persist :wheel"
+touch /etc/doas.conf && echo "permit persist :wheel" > /etc/doas.conf
 systemctl enable systemd-networkd.service
 systemctl enable systemd-resolved.service
-systemctl enable fstrim.service
+sed -i.bkp 's/#NTP=/NTP=time.stdtime.gov.tw/' /etc/systemd/timesyncd.conf && timedatectl set-ntp true
 systemctl enable apparmor.service
 systemctl enable ufw.service
-systemctl enable fstrim.timer
+#systemctl enable fstrim.timer
+#systemctl enable fstrim.service
 # install bootloader, configure systemd-boot
 bootctl install &&
 cp /usr/share/systemd/bootctl/arch.conf /boot/loader/entries/

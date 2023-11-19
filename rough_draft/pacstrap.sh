7bce68c67c92ed7296f508b05b9d51abd998f4e1
Jeff-WuYo <62562200+Jeff-WuYo@users.noreply.github.com>
2023-11-20 00:07:24 +0800

    fix typo

--- rough_draft/pacstrap.sh
+++ rough_draft/pacstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 pacstrap -K /mnt base linux linux-firmware intel-ucode udisks2 man vim zsh reflector git xdg-user-dirs p7zip usbutils polkit opendoas zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting starship
 genfstab -U /mnt >> /mnt/etc/fstab
-cp -r /etc/systmed/network/ /mnt/etc/systemd/
+cp -r /etc/systemd/network/ /mnt/etc/systemd/

f4f8934d6f09aa6b92e5812fadafe27f24d5a326
Jeff-WuYo <62562200+Jeff-WuYo@users.noreply.github.com>
2023-11-25 00:00:23 +0800

    quick mount for repairing

---
+++ scripts/quick-mount-for-system-repair.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# this script mount the btrfs system as originaly installed.
+read -p "What disk did os install to?: " dis
+if [[ "$dis" =~ "nvme" ]]; then
+    par="$dis"p
+else
+    par="$dis"
+fi
+rpar="$dis"3
+umount -A -R /mnt &&
+mount -o rw,noatime,compress=zstd,subvolid=256 /dev/$rpar /mnt &&
+mount -o rw,noatime,compress=zstd,subvolid=257 /dev/$rpar /mnt/home &&
+mount -o rw,noatime,compress=zstd,subvolid=258 /dev/$rpar /mnt/var/cache &&
+mount -o rw,noatime,compress=zstd,subvolid=259 /dev/$rpar /mnt/var/log &&
+mount -o rw,noatime,compress=zstd,subvolid=260 /dev/$rpar /mnt/var/lib/libvirt/images &&
+mount /dev/"$par"1 /mnt/efi &&
+mount /dev/"$par"2 /mnt/boot &&
+echo "Mounting done."

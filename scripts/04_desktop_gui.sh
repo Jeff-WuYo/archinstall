6bfb48904d34f4ab40d82d0ff50cdc2a16d17731
Jeff-WuYo <62562200+Jeff-WuYo@users.noreply.github.com>
2023-11-30 15:20:32 +0800

    add some packages

--- scripts/04_desktop_gui.sh
+++ scripts/04_desktop_gui.sh
@@ -2,8 +2,8 @@
 # install xorg and pipewire
 pacman -Sy xorg-server xorg-xhost xorg-xinit xorg-xrandr xorg-xkill alsa-utils pipewire pipewire-alsa pipewire-pulse wireplumber --needed --noconfirm
 # install utils
-pacman -S udisks2 wget usbutils playerctl duf yt-dlp flameshot ffmpeg perl-rename --needed --noconfirm
+pacman -S udisks2 wget usbutils playerctl duf yt-dlp flameshot ffmpeg perl-rename polikt-gnome --needed --noconfirm
 # install qtile and GUI apps
 pacman -S alacritty qtile python-xlib python-psutil lm_sensors nitrogen picom lxappearance-gtk3 gpicview libheif pcmanfm libheif galculator atril mpv chezmoi --needed --noconfirm
-# install input method, theme, icon
-pacman -S ibus-chewing breeze-icons --needed --noconfirm
+# install input method, theme, icon, fonts
+pacman -S ibus-chewing breeze-icons noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-noto-nerd --needed --noconfirm

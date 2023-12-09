b83c5c6938e5ff991754e69587b1a6fe6785b94f
Jeff-WuYo <62562200+Jeff-WuYo@users.noreply.github.com>
2023-12-10 02:38:02 +0800

    fix typo

--- startup.sh
+++ startup.sh
@@ -8,7 +8,7 @@ if [ "$(basename "$(pwd)")" = archinstall ]; then
     source ./scripts/02_pacstrap.sh &&
     cp -r ./configs/etc/ /mnt/
     mkdir -p /mnt/efi/EFI/systemd/drivers/ &&
-    cp -r /usr/lib/efifs-x64/ext2_64.efi /usr/lib/efifs-x64/btrfs_x64.efi /usr/lib/efifs-x64/xfs_x64.efi /mnt/efi/EFI/systemd/drivers/
+    cp -r /usr/lib/efifs-x64/ext2_x64.efi /usr/lib/efifs-x64/btrfs_x64.efi /usr/lib/efifs-x64/xfs_x64.efi /mnt/efi/EFI/systemd/drivers/
     mkdir -p /mnt/root/archinstall/scripts &&
     cp ./scripts/03_configure_system.sh ./scripts/04_desktop_gui.sh /mnt/root/archinstall/scripts/ &&
     arch-chroot /mnt /root/archinstall/scripts/03_configure_system.sh

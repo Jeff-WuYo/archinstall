9e8b12a2182f4e3fe32a58ff38733681f6c08905
Jeff-WuYo <62562200+Jeff-WuYo@users.noreply.github.com>
2024-03-07 16:59:38 +0800

    change ibus to fcitx5

--- scripts/04_desktop_gui.sh
+++ scripts/04_desktop_gui.sh
@@ -6,4 +6,4 @@ pacman -S udisks2 wget usbutils playerctl duf yt-dlp flameshot ffmpeg perl-renam
 # install qtile and GUI apps
 pacman -S alacritty qtile python-xlib python-psutil lm_sensors nitrogen picom lxappearance-gtk3 gpicview libheif pcmanfm libheif galculator atril mpv chezmoi --needed --noconfirm
 # install input method, theme, icon, fonts
-pacman -S ibus-chewing breeze-icons noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-noto-nerd --needed --noconfirm
+pacman -S fcitx5-chewing fcitx5-im breeze-icons noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-noto-nerd --needed --noconfirm

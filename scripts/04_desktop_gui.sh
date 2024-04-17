1ef20e1f4db3d702acecfd46d86e13a282193966
Jeff-WuYo <62562200+Jeff-WuYo@users.noreply.github.com>
2024-04-17 10:33:06 +0800

    replace xinit with sx

--- scripts/04_desktop_gui.sh
+++ scripts/04_desktop_gui.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # install xorg and pipewire
-pacman -Sy xorg-server xorg-xhost xorg-xinit xorg-xrandr xorg-xkill alsa-utils pipewire pipewire-alsa pipewire-pulse wireplumber --needed --noconfirm
+pacman -Sy sx xorg-xhost xorg-xrandr xorg-xkill alsa-utils pipewire pipewire-alsa pipewire-pulse wireplumber --needed --noconfirm
 # install utils
 pacman -S udisks2 wget usbutils playerctl duf yt-dlp flameshot ffmpeg perl-rename polikt-gnome --needed --noconfirm
 # install qtile and GUI apps
-pacman -S alacritty qtile python-xlib python-psutil lm_sensors nitrogen picom lxappearance-gtk3 gpicview libheif pcmanfm libheif galculator atril mpv chezmoi --needed --noconfirm
+pacman -S alacritty qtile python-xlib python-psutil lm_sensors nitrogen picom lxappearance-gtk3 gpicview libheif pcmanfm galculator atril mpv chezmoi --needed --noconfirm
 # install input method, theme, icon, fonts
 pacman -S fcitx5-chewing fcitx5-im breeze-icons noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-noto-nerd --needed --noconfirm

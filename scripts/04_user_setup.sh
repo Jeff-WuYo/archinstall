f3f4685f50ad8107a06cd853098a0326f983f7c1
Jeff-WuYo <62562200+Jeff-WuYo@users.noreply.github.com>
2024-05-02 18:04:47 +0800

    fix password creation error

--- scripts/04_user_setup.sh
+++ scripts/04_user_setup.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 set -euo pipefail
-unset -v username uid fullname groupname gid shell ans4 sudogroup
+unset -v username uid fullname groupname gid shell ans4
 passwduid=$(cut -f 3 -d: /etc/passwd | sort -n | tail -2 | head -1)
 [ "$passwduid" -lt 1000 ] && defaultuid=1000 || defaultuid=$(( $passwduid + 1 ))
 groupgid=$(cut -f 3 -d: /etc/group | sort -n | tail -2 | head -1)
 [ "$groupgid" -lt 1000 ] && defaultgid=1000 || defaultgid=$(( $groupgid + 1 ))
+function passwd_check {
+  while [ $? -ne 0 ]; do
+    echo "Please enter your password again"
+    passwd "$username" || passwd_check
+  done
+}
 
 read -p "What's your username: " username
 while [ -z "$username" ]; do
@@ -15,9 +21,6 @@ read -ep "What's your uid? [$defaultuid]: " -i "$defaultuid" uid
 while [[ ! "$uid" =~ ^[0-9]+$ || "$uid" -lt "$passwduid" ]]; do
   read -ep "Please enter uid with number at least or greater than [$defaultuid]: " -i "$defaultuid" uid
 done
-
-read -p "What's your fullname: " fullname
-[ -z "$fullname" ] && unset -v setfullname || setfullname="-c $fullname"
 
 read -ep "What's your groupname [$username]: " -i "$username" groupname
 while [ -z "$groupname" ]; do
@@ -35,12 +38,12 @@ while [[ ! "$shell" = "bash" && ! "$shell" = "zsh" ]]; do
 done
 
 read -p "Do you want to add $username to wheel group? " ans4
-[ "$ans4" = yes ] && sudogroup="-G wheel" || unset -v sudogroup
 
 groupadd -g "$gid" "$groupname" &&
-useradd -m -u "$uid" -g "$gid" "$sudogroup" "$setfullname" -s "/usr/bin/$shell" "$username"
-passwd "$username" ||;
-while [ $? -ne 0 ]; do
-  echo "Please enter password again"
-  passwd "$username"
-done
+if [ "$ans4" = yes ]; then
+  useradd -m -u "$uid" -g "$gid" -G wheel -s "/usr/bin/$shell" "$username"
+else
+  useradd -m -u "$uid" -g "$gid" -s "/usr/bin/$shell" "$username"
+fi
+
+passwd "$username" || passwd_check

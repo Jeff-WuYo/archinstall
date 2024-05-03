cdce40cd51d9f8a4a15b8c2b1431ed2e2ee22850
Jeff-WuYo <62562200+Jeff-WuYo@users.noreply.github.com>
2024-05-04 01:54:34 +0800

    add bigger and prettier tty font

--- scripts/02_pacstrap.sh
+++ scripts/02_pacstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-pacstrap -K /mnt base linux linux-firmware intel-ucode udisks2 man vim reflector usbutils polkit opendoas apparmor ufw git xdg-user-dirs mold pigz lbzip2 p7zip zram-generator zsh grml-zsh-config
+pacstrap -K /mnt base linux linux-firmware terminus-font intel-ucode udisks2 man vim reflector usbutils polkit opendoas apparmor ufw git xdg-user-dirs mold pigz lbzip2 p7zip zram-generator zsh grml-zsh-config
 genfstab -U /mnt >> /mnt/etc/fstab
 cp -r /etc/systemd/network/ /mnt/etc/systemd/

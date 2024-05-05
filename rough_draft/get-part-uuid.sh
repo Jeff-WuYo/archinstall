96715abcc124724ad5e8cb577ad1040c433079e7
Jeff-WuYo <62562200+Jeff-WuYo@users.noreply.github.com>
2024-05-05 21:43:50 +0800

    script to get UUID or PARTUUID for my systemd-boot setup

---
+++ rough_draft/get-part-uuid.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+#original command
+echo "↓↓↓↓↓original command↓↓↓↓↓"
+time blkid | awk '/'"$(awk '/256/ {print $1}' /etc/fstab | tr -d UUID=)"'/ {print$NF}' | tr -d \"
+echo "↓↓↓↓↓new comand↓↓↓↓↓"
+time cut -d' ' -f 3 <(grep $(cut -c 6-41 <(grep "256" /etc/fstab)) <(blkid -s UUID -s PARTUUID)) | tr -d \"
+echo "↓↓↓↓↓new new command, which only gets UUID↓↓↓↓↓"
+time cut -f1 <(grep "256" /etc/fstab)
+echo "↓↓↓↓↓new new command, but with awk↓↓↓↓↓"
+time awk '/256/ {print $1}' /etc/fstab

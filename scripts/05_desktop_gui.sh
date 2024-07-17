3ab50443727eaf7d1c0926e4b79b678efe854299
Jeff-WuYo <62562200+Jeff-WuYo@users.noreply.github.com>
2024-07-17 11:53:59 +0800

    change to pcmanfm-qt

--- scripts/05_desktop_gui.sh
+++ scripts/05_desktop_gui.sh
@@ -4,6 +4,6 @@ pacman -Sy sx xorg-xhost xorg-xrandr xorg-xkill alsa-utils pipewire pipewire-als
 # install utils
 pacman -S udisks2 wget usbutils playerctl duf yt-dlp flameshot ffmpeg perl-rename polikt-gnome zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting starship --needed --noconfirm
 # install qtile and GUI apps
-pacman -S alacritty qtile python-xlib python-psutil lm_sensors nitrogen picom lxappearance-gtk3 gpicview libheif pcmanfm galculator atril mpv chezmoi --needed --noconfirm
+pacman -S alacritty qtile python-xlib python-psutil lm_sensors nitrogen picom lxappearance-gtk3 gpicview libheif pcmanfm-qt galculator atril mpv chezmoi --needed --noconfirm
 # install input method, theme, icon, fonts
 pacman -S fcitx5-chewing fcitx5-im breeze-icons noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-noto-nerd --needed --noconfirm

50d5ab418d1f599f048917f9c0b6eafdf828fa0d
Jeff-WuYo <62562200+Jeff-WuYo@users.noreply.github.com>
2024-09-01 20:27:25 +0800

    rewrite desktop setup

--- scripts/05_desktop_gui.sh
+++ scripts/05_desktop_gui.sh
@@ -1,9 +1,36 @@
 #!/usr/bin/env bash
-# install xorg and pipewire
-pacman -Sy sx xorg-xhost xorg-xrandr xorg-xkill alsa-utils pipewire pipewire-alsa pipewire-pulse wireplumber --needed --noconfirm
-# install utils
-pacman -S udisks2 wget usbutils playerctl duf yt-dlp flameshot ffmpeg perl-rename polikt-gnome zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting starship --needed --noconfirm
-# install qtile and GUI apps
-pacman -S alacritty qtile python-xlib python-psutil lm_sensors nitrogen picom lxappearance-gtk3 gpicview libheif pcmanfm-qt galculator atril mpv chezmoi --needed --noconfirm
-# install input method, theme, icon, fonts
-pacman -S fcitx5-chewing fcitx5-im breeze-icons noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-noto-nerd --needed --noconfirm
+qtile_xorg() {
+  pacman -S sx xorg-xhost xorg-xkill picom qtile flameshot xorg-xrandr --needed --noconfirm
+  pacman -S --asdeps alsa-utils libpulse lm_sensors python-dbus-next python-psutil python-setproctitle python-xdg --needed --noconfirm
+}
+
+qtile_wayland() {
+  pacman -S qtile grim swayidle wlr-randr --needed --noconfirm
+  pacman -S --asdeps alsa-utils libinput libpulse lm_sensors python-dbus-next python-psutil python-pywayland python-pywlroots python-setproctitle python-xdg python-xkbcommon xorg-xwayland xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-wlr slurp --needed --noconfirm
+}
+
+river() {
+  pacman -S river grim swayidle wlr-randr --needed --noconfirm
+  pacman -S --asdeps xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-wlr slurp --needed --noconfirm
+}
+
+utils() {
+  pacman -S alacritty udisks2 wget usbutils playerctl perl-rename polikt-gnome zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting zoxide neovim chezmoi starship --needed --noconfirm
+}
+
+others() {
+  pacman -S fcitx5-chewing fcitx5-im papirus-icon-theme noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-noto-nerd yt-dlp mpv galculator atril pcmanfm-qt nwg-look --needed --noconfirm
+  pacman -S --asdeps lxqt-archiver gvfs-mtp fzf --needed --noconfirm
+}
+
+wm_list=( qtile_xorg qtile_wayland none )
+
+printf "Which WM do you want to install?\n"
+select wm in "${wm_list[@]}"; do
+  case $wm in
+    qtile_xorg) qtile_xorg ; utils ; others ; break ;;
+    qtile_wayland) qtile_wayland ; utils ; others ; break ;;
+    none) printf "Done!\n" ; exit 0 ;;
+    *) printf "Invalid choise, please try again. \n"
+  esac
+done
